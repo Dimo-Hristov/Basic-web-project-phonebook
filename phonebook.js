17cbbc4caaec746e832c54b8f71a4b3b5e0da6ef
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-12-06 22:10:32 +0100

    Update phonebook.js

--- phonebook.js
+++ phonebook.js
@@ -1,5 +1,19 @@
+const phonebook = [
+	{
+		name: 'pesho',
+		number: '1234'
+	},
+	{
+		name: 'gosho',
+		number: '12345'
+	}
+];
+
 /* TODO: 
 	create phonebook array
 	add methods for adding in the phonebook and getting it
 	export the methods
 */
+function getContacts() {
+	return phonebook.slice();
+}
